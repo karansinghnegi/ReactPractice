7193e9c0de20cfbebc404c2732a290f445a1eb58
karan <lost.cause.ksn@gmail.com>
2022-07-24 15:56:30 +0530

    React v7

---
+++ 1. React FrontEndMasters/1. Intro to React V17/4. JSX Intro/src/App.js
@@ -0,0 +1,15 @@
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mix" />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
